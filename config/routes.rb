32a8a5194adf7491efab1db651feaf92ca6c0d47
Laskin00 <ale.laskintues19@gmail.com>
2017-10-29 18:45:01 +0200

    hw2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :texts
-  root 'text/index'
-  post 'text/index' => 'text#create'
+  root 'text#index'
+  post 'text' =>'text#create'
   get 'messages/:id' => 'text#read'
 end
