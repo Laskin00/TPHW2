3113cfa7ba1328215b3fff3d6c1847ad775b866c
Laskin00 <ale.laskintues19@gmail.com>
2017-11-05 13:25:27 +0200

    Hw3

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -6,7 +6,7 @@ class TextController < ApplicationController
   end
   def create
     $message = params[:text]
-    $heroku_link.to_s = "https://texthider.herokuapp.com/messages/"
+    $heroku_link = "https://texthider.herokuapp.com/messages/"
     Text.create(:text => $message, :number => $id)
     $id = $id + 1
     render plain: $heroku_link.to_s + $id.to_s
@@ -19,6 +19,6 @@ class TextController < ApplicationController
     $message = params[:message]
     Text.create(:text => $message, :number => $id)
     $id = $id + 1
-    render plain: $heroku_link.to_s.to_s + $id.to_s
+    render plain: $heroku_link.to_s + $id.to_s
   end
 end
