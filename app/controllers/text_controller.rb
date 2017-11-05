14369302d21f6b823896e47c2de94f8576f40569
Laskin00 <ale.laskintues19@gmail.com>
2017-11-05 16:27:43 +0200

    Hw3

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -1,23 +1,26 @@
 class TextController < ApplicationController
   require 'json'
   require 'net/http'
-  require 'nokogiri'
 
   $message = ""
   $id = 0
   def index
   end
+
   def create
     $message = params[:text]
     Text.create(:text => $message, :number => $id)
     $id = $id + 1
     render plain: "https://texthider.herokuapp.com/messages/" + $id.to_s
   end
+
   def read
       render plain: Text.find_by(:number => ($id - 1)).text
       Text.destroy_all
   end
+
   def c_json
+
     if request.headers["Content-Type"] == 'application/json'
       $message = params[:message]
       Text.create(:text => $message, :number => $id)
@@ -25,13 +28,14 @@ class TextController < ApplicationController
       url = "https://texthider.herokuapp.com/messages/" + $id.to_s
       render plain: "'"'{"url":' + '"' + url.to_s + '"}'"'" + "\n"
     end
+
     if request.headers["Content-Type"] == 'text/xml'
       $message = Nokogiri::XML(request.body.read).content
       Text.create(:text => $message, :number => $id)
       $id = $id + 1
       url = "https://texthider.herokuapp.com/messages/" + $id.to_s
-      render plain: '<url>' + url.to_s +'</url>' + "\n"
-
+      render plain: url.to_s
     end
+
   end
 end
